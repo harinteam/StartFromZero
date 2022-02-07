416db7bae7fe98700a5d956fa17f20506009796e
adityadenny <adityadenny6@gmail.com>
2022-02-08 01:21:39 +0700

    initial commit

--- main.py
+++ main.py
@@ -1,5 +1,25 @@
 """
 Ini adalah demo project pertama dengan python!
 """
-print("Hello World Denny")
-print("Let's learn Fundamental Python")
+
+# test hello word
+# print("Hello World Denny")
+# print("Let's learn Fundamental Python")
+
+"""
+Semua sintaksis dalam bahasa pemrograman terdiri dari :
+1. Sekuential : langkah berurutan
+2. Percabangan : langkah melompah jika kondisi terpenuhi
+3. Perulangan : mengulang langkah yang sama berkali-kali / sampai kondisi terpenuhi
+"""
+
+# Percabangan
+jumlah_botol_susu = 173
+
+if jumlah_botol_susu > 0:
+    print("Budi membeli 1 botol susu")
+else:
+    print("Budi tidak jadi membeli 1 botol susu")
+
+print("Budi pulang ke rumah")
+print("Menyampaikan hasilnya kepada ibu")
