5ff2dc13013309e2f1f8253778bff4c38a292769
adityadenny <adityadenny6@gmail.com>
2022-02-08 01:41:02 +0700

    practice Budi buy Bottle of milk and eggs

---
+++ buy_milk_eggs.py
@@ -0,0 +1,9 @@
+print('Mom Said : "Plase go to the shop and buy 1 bottle of milk, if they have eggs, bring 6"')
+
+bottle_of_milk = 17
+eggs_count = 12
+
+if bottle_of_milk > 0:
+    print("Budi buy 1 bottle of milk")
+if eggs_count > 0:
+    print("Budi buy 6 eggs")
