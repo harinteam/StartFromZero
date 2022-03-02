11e606a7dfe9f38fa5a2ee2ad81e8c810478ac79
adityadenny <aditaydenny6@gmail.com>
2022-03-02 23:11:48 +0700

    learn basic dictionary

--- dasar_sintaksis_dict.py
+++ dasar_sintaksis_dict.py
@@ -2,7 +2,19 @@ users = {
     'id': 1,
     'name': 'John Joselvi',
     'username': 'jonijon',
-    'email': 'joni1@gmail.com'
+    'email': 'joni1@gmail.com',
+    'address': {
+        'street': 'Holliwood Street',
+        'suite': 'Apt. 99',
+        'city': 'Tokyo',
+        'zipcode': '239120',
+        'geo': {
+            'lat': '-34.3159',
+            'lng': '81.149'
+        }
+    }
 }
 print(users)
 print(users['email'])
+print(users['address']['suite'])
+print(users['address']['geo']['lat'])
