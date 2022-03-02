b5d36b18162f5bc9d34bda5757ec006040fb1f0c
adityadenny <aditaydenny6@gmail.com>
2022-03-02 13:08:01 +0700

    belajar tipe data list comprehension & add new file dasar sintaksis dictionary

--- dasar_sintaksis_list_comprehension.py
+++ dasar_sintaksis_list_comprehension.py
@@ -25,3 +25,14 @@ book_titles = ['Atomic Habits', 'First Thing First', 'Ikigai', 'Steve Jobs', 'Or
 del book_titles[0::2] # START : END : STEP
 for i in range(0, len(book_titles)):
     print(book_titles[i])
+
+print('\n# Membuat List baru new book titles copy data dri book_titles')
+book_titles = ['Atomic Habits', 'First Thing First', 'Ikigai', 'Steve Jobs', 'Origami', 'Hitsimishu']
+new_book_titles = book_titles[:]
+for i in range(0, len(book_titles)):
+    print(book_titles[i])
+
+print('\n# Membuat list baru, tampilkan, dan hapus list lama book_titles')
+del book_titles[:]
+for i in range(0, len(new_book_titles)):
+    print(new_book_titles[i])
