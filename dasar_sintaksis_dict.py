e5e6b1bd818be4e6dba95bc9a8041a22631526f8
adityadenny <aditaydenny6@gmail.com>
2022-03-03 00:28:01 +0700

    learn basic dictionary

--- dasar_sintaksis_dict.py
+++ dasar_sintaksis_dict.py
@@ -18,3 +18,16 @@ print(users)
 print(users['email'])
 print(users['address']['suite'])
 print(users['address']['geo']['lat'])
+
+print('\n')
+print(users)
+print(type(users))
+
+print('\n# Ubah dict ke json')
+import json
+result = json.dumps(users)
+print(type(result))
+print(result)
+
+with open('result.json', 'w') as file:
+    json.dump(users, file)
